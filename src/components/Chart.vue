7d593ac91c9ac60260c7f92c01d94fe1fcdeff37
Prem Lamsal <premlamsal2@gmail.com>
2023-08-02 22:10:17 +0545

    char working

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -14,21 +14,22 @@ import { onMounted, ref, inject, reactive } from "vue";
 
 const myChart = ref(null);
 const axios = inject("$axios");
+
+//charts start
+const chartDataSets = ref([
+  {
+    label: "# of Sales",
+    data: [],
+    borderWidth: 1,
+  },
+]);
+const chartDataLabels = ref(["2080-09", "2080-09"]);
+
 const chartData = reactive({
-  labels: ["JAN", "FEB", "MAR", "JUN", "JULY", "AUG"],
-  datasets: [
-    {
-      label: "# of Purchase",
-      data: [12, 19, 3, 5, 2, 3],
-      borderWidth: 1,
-    },
-    // {
-    //   label: "# of Sales",
-    //   data: [9, 19, 50, 90, 100, 200],
-    //   borderWidth: 1,
-    // },
-  ],
+  labels: chartDataLabels,
+  datasets: chartDataSets,
 });
+
 const chartOptions = ref({
   responsive: true,
   maintainAspectRatio: true,
@@ -38,33 +39,53 @@ const chartOptions = ref({
     },
   },
 });
-const chartType = ref("bar");
-
-onMounted(() => {
-  console.log(chartType.value);
-  console.log(chartOptions);
-  console.log(chartData);
-
-  getSalesChartInfo();
-  const ctx = myChart.value;
-  new Chart(ctx, {
-    type: chartType.value,
-    data: chartData,
-    options: chartOptions,
-  });
-});
-
+const chartType = ref("line");
+//charts ends
+const getMonthName = (date_month) => {
+  return new Intl.DateTimeFormat("en-US", {
+    month: "long",
+  }).format(new Date(date_month));
+};
 const getSalesChartInfo = () => {
   axios
     .get("sales-chart-info/" + 6)
     .then((response) => {
       //   console.log(response.data.month);
       //   console.log(response.data.data);
+      // chartDataSets.value[0].label = "# of Sales";
+      for (let i = 0; i < response.data.month.length; i++) {
+        // chartDataLabels.value[i] = response.data.month[i];
+        chartDataLabels.value[i] = getMonthName(response.data.month[i]);
+      }
+      for (let i = 0; i < response.data.month.length; i++) {
+        //this will assign to 0th data array for chart and ith items to data itself
+        chartDataSets.value[0].data[i] = response.data.data[i];
+      }
+
+      // console.log(chartDataLabels);
+      //   console.log(chartDataSets.value[0].label);
+      //   console.log(chartDataSets.value[1].label);
+      console.log(chartDataSets.value[0].data);
+      //   console.log(chartDataSets.value[1].data);
+
+      makeSalesChart();
     })
     .catch((error) => {
       console.log(error);
     });
 };
+const makeSalesChart = () => {
+  const ctx = myChart.value;
+  new Chart(ctx, {
+    type: chartType.value,
+    data: chartData,
+    options: chartOptions,
+  });
+};
+
+onMounted(() => {
+  getSalesChartInfo();
+});
 </script>
 
 <style scoped>
